a86d683e8b3f6a80fc4ad731bb6bc43055221e23
Harsh Girdhar <girdharharsh01@gmail.com>
2019-01-22 14:45:04 -0700

    Added exceptions Model & Http NotfoundException

---
+++ app/Exceptions/ExceptionTrait.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+trait ExceptionTrait
+{
+    public function apiException($request, $e) {
+        if($this->isModel($e)) {
+            return $this->ModelResponse($e);
+        }
+        if($this->isHTTP($e)) {
+            return $this->HttpResponse($e);
+        }
+
+        return parent::render($request, $e);
+    }
+
+    public function isModel($e) {
+        return $e instanceof ModelNotFoundException;
+    }
+
+    public function isHTTP($e) {
+        return $e instanceof NotFoundHttpException;
+    }
+
+    public function ModelResponse($e) {
+        return response()->json([
+            'errors' => 'Product model not found'
+        ], Response::HTTP_NOT_FOUND);
+    }
+
+    public function HttpResponse($e) {
+        return response()->json([
+            'errors' => 'Incorrect route'
+        ], Response::HTTP_NOT_FOUND);
+    }
+}
